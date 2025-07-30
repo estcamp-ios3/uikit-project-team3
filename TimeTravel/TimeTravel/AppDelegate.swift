4af29d45f37ac9b19a3baf3bf72ded0995c67f28
coco <slowly0_0@naver.com>
2025-07-30 14:38:53 +0900

    Tt
    tt

--- TimeTravel/TimeTravel/AppDelegate.swift
+++ TimeTravel/TimeTravel/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+// 야니아러ㅑㄷ지낭러;ㅁ니아ㅡ
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
