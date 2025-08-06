54960b5780451bff7ed805b599d2278d8b7dfe35
dwkim <dongwoo.kim124@gmail.com>
2025-08-06 17:15:00 +0900

    mapview, prologueview, mapviewcontroller, prologueviewcontroller
    mapview, prologueview, mapviewcontroller, prologueviewcontroller changes

--- LociTravel/LociTravel/ViewController/PrologueViewController.swift
+++ LociTravel/LociTravel/ViewController/PrologueViewController.swift
@@ -14,10 +14,16 @@ class PrologueViewController: UIViewController {
         view = prologueView
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        skipButtonActions()
         
+    }
+    
+    // viewdidappear 추가
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        prologueView.startCreditsAnimation()   // ← 여기로 이동
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -25,4 +31,14 @@ class PrologueViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
+    private func skipButtonActions() {
+        prologueView.skipButton.addTarget(self, action: #selector(didTapSkipButton), for: .touchUpInside)
+    }
+    
+    //넘어가기 버튼 함수
+    @objc private func didTapSkipButton() {
+        
+        let mapVC = MapViewController()
+        navigationController?.pushViewController(mapVC, animated: true)
+    }
 }
